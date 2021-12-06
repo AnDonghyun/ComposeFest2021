f40a04ddbaf0aa11005f470d0fb9f9c99a3fa57d
안동현 <miehdan@gmail.com>
2021-12-07 01:47:08 +0900

    5. Hello Compose!

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -17,10 +17,12 @@
 package com.google.samples.apps.sunflower.plantdetail
 
 import android.content.Intent
+import android.opengl.Matrix
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -66,6 +68,12 @@ class PlantDetailFragment : Fragment() {
                         Snackbar.make(root, R.string.added_plant_to_garden, Snackbar.LENGTH_LONG)
                             .show()
                     }
+                }
+            }
+
+            composeView.setContent {
+                MaterialTheme {
+                    PlantDetailDescription()
                 }
             }
 
